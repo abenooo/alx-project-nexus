40a36ef04e7cd04c895a0e0efc1b90e67bed8e2e
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-08-08 02:14:16 -0700

    Remove unused theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Navbar } from '@/components/navbar'
-import { ThemeProvider } from '@/components/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,15 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
+        
           <Navbar />
           <main>{children}</main>
-        </ThemeProvider>
       </body>
     </html>
   )
